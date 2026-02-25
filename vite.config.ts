2d4f2aa207d461332859b07916027b5e86d700e2
Archit Tayal <archittayal485@gmail.com>
2026-02-25 21:36:34 +0530

    smaller chunk sizes

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,27 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     base: env.GITHUB_ACTIONS || env.VERCEL_GITHUB_DEPLOYMENT ? '/MyAdmin/' : '/',
+    build: {
+      chunkSizeWarningLimit: 1000,
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              if (id.includes('react') || id.includes('react-dom') || id.includes('react-router')) {
+                return 'vendor-react';
+              }
+              if (id.includes('@supabase')) {
+                return 'vendor-supabase';
+              }
+              if (id.includes('framer-motion')) {
+                return 'vendor-animation';
+              }
+              return 'vendor'; // Fallback for other node_modules
+            }
+          }
+        }
+      }
+    }
   }
 })
 
